6b0c509d9b14f06ed22add55608b7abcc3b18529
iyousojo <solomonjohnbull676@gmail.com>
2026-03-03 11:15:53 +0100

    `Updated User model schema and save hook`

--- src/models/User.js
+++ src/models/User.js
@@ -33,13 +33,12 @@ const userSchema = new mongoose.Schema({
     }
 }, { timestamps: true });
 
-userSchema.pre('save', async function(next) {
+userSchema.pre('save', async function() {
     if (!this.isModified('password')) {
-        return next(); // IMPORTANT: return
+        return; // Exit early if password isn't changed
     }
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
-    next();
 });
 
 userSchema.methods.matchPassword = async function(enteredPassword) {
